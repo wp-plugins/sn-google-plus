7e6dcd748a74b92226147ce6dbb0b855ea3d5154
Retio <Retio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-19 10:05:04 +0000

    v.1.4.2
    git-svn-id: https://plugins.svn.wordpress.org/sn-google-plus/tags/1.4.2@700186 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sn-google-plus.php
+++ sn-google-plus.php
@@ -1,11 +1,11 @@
 <?php
 /*
   Plugin Name: SN Google Plus
-  Plugin URI: http://mateuszlerczak.com/sn-google-plus/
+  Plugin URI: http://wp-extend.info/plugins/google/sn-google-plus/
   Description: SN Google Plus button.
   Author: Retio
-  Author URI: http://mateuszlerczak.com
-  Version: 1.4
+  Author URI: http://blog.lerczak.eu
+  Version: 1.4.2
   Text Domain: SNGP
   Domain Path: /lang/
  */
